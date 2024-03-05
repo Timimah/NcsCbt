8393ac470bd2780941d0b3a8f658ec9108c90026
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-05 23:27:24 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import { ThemeUIProvider } from 'theme-ui'
-import { theme } from './theme'
-import { useColorMode } from 'theme-ui'
-
+import { MyRoutes } from './router/MyRoutes'
 
 const App = () => {
-  const [colorMode, setColorMode] = useColorMode()
 
   return (
-    <ThemeUIProvider theme={theme}>
-      <h1
-        sx={{
-          color: 'primary',
-          fontFamily: 'heading',
-        }}
-      >
-        Hello
-      </h1>
-      <button
-        // onClick={(e) => {
-        //   setColorMode(colorMode === 'default' ? 'dark' : 'default')
-        // }}
-        // sx ={{
-        //   backgroundColor: 'black'
-        // }}
-      >
-        Toggle  {colorMode === 'default' ? 'Dark' : 'Light'}
-      </button>
-    </ThemeUIProvider>
+    <div className='absolute top-0 bottom-0 left-0 right-0'>
+      <MyRoutes />
+    </div>
   )
 }
 
