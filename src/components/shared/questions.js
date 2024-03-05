aee2273456f0ce42c08cce8831a671f87eb0ee16
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-05 23:30:50 +0100

    add questions

---
+++ src/components/shared/questions.js
@@ -0,0 +1,203 @@
+export const questions = [
+    {
+        question: "What is the capital of France?",
+        answer: "Paris",
+        options: [
+            {value: "New York", label: "A. New York"},
+            {value: "London", label: "B. London"},
+            {value: "Paris", label: "C. Paris"},
+            {value: "Dublin", label: "D. Dublin"}
+        ]
+    },
+    {
+        question: "Who is CEO of Tesla?",
+        answer: "Elon Musk",
+        options: [
+            {value: "Jeff Bezos", label: "A. Jeff Bezos"},
+            {value: "Elon Musk", label: "B. Elon Musk"},
+            {value: "Bill Gates", label: "C. Bill Gates"},
+            {value: "Tony Stark", label: "D. Tony Stark"}
+        ]           
+    },
+    {
+        question: "The iPhone was created by which company?",
+        answer: "Apple",
+        options: [
+            {value: "Apple", label: "A. Apple"},
+            {value: "Intel", label: "B. Intel"},
+            {value: "Amazon", label: "C. Amazon"},
+            {value: "Microsoft", label: "D. Microsoft"}
+        ]
+    },
+    {
+        question: "How many Harry Potter books are there?",
+        answer: "7",
+        options: [
+            {value: "1", label: "A. 1"},
+            {value: "4", label: "B. 4"},
+            {value: "6", label: "C. 6"},
+            {value: "7", label: "D. 7"}
+        ]
+    },
+    {
+        question: "What is the capital of France?",
+        answer: "Paris",
+        options: [
+            {value: "New York", label: "A. New York"},
+            {value: "London", label: "B. London"},
+            {value: "Paris", label: "C. Paris"},
+            {value: "Dublin", label: "D. Dublin"}
+        ]
+    },
+    {
+        question: "Who is CEO of Tesla?",
+        answer: "Elon Musk",
+        options: [
+            {value: "Jeff Bezos", label: "A. Jeff Bezos"},
+            {value: "Elon Musk", label: "B. Elon Musk"},
+            {value: "Bill Gates", label: "C. Bill Gates"},
+            {value: "Tony Stark", label: "D. Tony Stark"}
+        ]           
+    },
+    {
+        question: "The iPhone was created by which company?",
+        answer: "Apple",
+        options: [
+            {value: "Apple", label: "A. Apple"},
+            {value: "Intel", label: "B. Intel"},
+            {value: "Amazon", label: "C. Amazon"},
+            {value: "Microsoft", label: "D. Microsoft"}
+        ]
+    },
+    {
+        question: "How many Harry Potter books are there?",
+        answer: "7",
+        options: [
+            {value: "1", label: "A. 1"},
+            {value: "4", label: "B. 4"},
+            {value: "6", label: "C. 6"},
+            {value: "7", label: "D. 7"}
+        ]
+    },
+    {
+        question: "What is the capital of France?",
+        answer: "Paris",
+        options: [
+            {value: "New York", label: "A. New York"},
+            {value: "London", label: "B. London"},
+            {value: "Paris", label: "C. Paris"},
+            {value: "Dublin", label: "D. Dublin"}
+        ]
+    },
+    {
+        question: "Who is CEO of Tesla?",
+        answer: "Elon Musk",
+        options: [
+            {value: "Jeff Bezos", label: "A. Jeff Bezos"},
+            {value: "Elon Musk", label: "B. Elon Musk"},
+            {value: "Bill Gates", label: "C. Bill Gates"},
+            {value: "Tony Stark", label: "D. Tony Stark"}
+        ]           
+    },
+    {
+        question: "The iPhone was created by which company?",
+        answer: "Apple",
+        options: [
+            {value: "Apple", label: "A. Apple"},
+            {value: "Intel", label: "B. Intel"},
+            {value: "Amazon", label: "C. Amazon"},
+            {value: "Microsoft", label: "D. Microsoft"}
+        ]
+    },
+    {
+        question: "How many Harry Potter books are there?",
+        answer: "7",
+        options: [
+            {value: "1", label: "A. 1"},
+            {value: "4", label: "B. 4"},
+            {value: "6", label: "C. 6"},
+            {value: "7", label: "D. 7"}
+        ]
+    },
+    {
+        question: "What is the capital of France?",
+        answer: "Paris",
+        options: [
+            {value: "New York", label: "A. New York"},
+            {value: "London", label: "B. London"},
+            {value: "Paris", label: "C. Paris"},
+            {value: "Dublin", label: "D. Dublin"}
+        ]
+    },
+    {
+        question: "Who is CEO of Tesla?",
+        answer: "Elon Musk",
+        options: [
+            {value: "Jeff Bezos", label: "A. Jeff Bezos"},
+            {value: "Elon Musk", label: "B. Elon Musk"},
+            {value: "Bill Gates", label: "C. Bill Gates"},
+            {value: "Tony Stark", label: "D. Tony Stark"}
+        ]           
+    },
+    {
+        question: "The iPhone was created by which company?",
+        answer: "Apple",
+        options: [
+            {value: "Apple", label: "A. Apple"},
+            {value: "Intel", label: "B. Intel"},
+            {value: "Amazon", label: "C. Amazon"},
+            {value: "Microsoft", label: "D. Microsoft"}
+        ]
+    },
+    {
+        question: "How many Harry Potter books are there?",
+        answer: "7",
+        options: [
+            {value: "1", label: "A. 1"},
+            {value: "4", label: "B. 4"},
+            {value: "6", label: "C. 6"},
+            {value: "7", label: "D. 7"}
+        ]
+    },
+    {
+        question: "What is the capital of France?",
+        answer: "Paris",
+        options: [
+            {value: "New York", label: "A. New York"},
+            {value: "London", label: "B. London"},
+            {value: "Paris", label: "C. Paris"},
+            {value: "Dublin", label: "D. Dublin"}
+        ]
+    },
+    {
+        question: "Who is CEO of Tesla?",
+        answer: "Elon Musk",
+        options: [
+            {value: "Jeff Bezos", label: "A. Jeff Bezos"},
+            {value: "Elon Musk", label: "B. Elon Musk"},
+            {value: "Bill Gates", label: "C. Bill Gates"},
+            {value: "Tony Stark", label: "D. Tony Stark"}
+        ]           
+    },
+    {
+        question: "The iPhone was created by which company?",
+        answer: "Apple",
+        options: [
+            {value: "Apple", label: "A. Apple"},
+            {value: "Intel", label: "B. Intel"},
+            {value: "Amazon", label: "C. Amazon"},
+            {value: "Microsoft", label: "D. Microsoft"}
+        ]
+    },
+    {
+        question: "How many Harry Potter books are there?",
+        answer: "7",
+        options: [
+            {value: "1", label: "A. 1"},
+            {value: "4", label: "B. 4"},
+            {value: "6", label: "C. 6"},
+            {value: "7", label: "D. 7"}
+        ]
+    }
+    
+];
