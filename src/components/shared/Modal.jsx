06163891a7a05f0d455fa9a7f3855450fc021239
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-05 23:56:40 +0100

    feat: create reusable Modal component

---
+++ src/components/shared/Modal.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Button } from './Button';
+
+export const Modal = ({ title, content, modStyles, buttons }) => {
+
+    return (
+        <div className="absolute inset-0 top-0 bottom-0 left-0 right-0 p-8 flex items-center justify-center w-full z-10 h-full">
+            <div className={`relative rounded-lg max-w-full h-full shadow-lg transform transition-all duration-300 p-8 ${modStyles}`}>
+                {/* Modal Header */}
+                <div className="flex justify-between items-center">
+                    <h2 className="text-2xl font-bold text-primary -ml-2">{title}</h2>
+                </div>
+                {/* Modal Content */}
+                <div className="flex flex-col w-full">
+                    <div className="">{content}</div>
+                    {buttons}
+                </div>
+            </div>
+        </div>
+    );
+};
