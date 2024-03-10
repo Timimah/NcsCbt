1ee9c775c7cff9cf9aa6482adc750b8001b2e877
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-10 19:36:14 +0100

    feat: create Card component with icon, title, and content props

---
+++ src/components/website/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Card = ({icon, title, content}) => {
+  return (
+    <div className='bg-cardgreen shadow-md p-5 rounded-br-xl text-left text-white flex flex-col gap-4'>
+        <div className='p-2 bg-white rounded-full h-10 w-10 flex items-center justify-center'>
+            {icon}
+        </div>
+        <div className='text-sm'>{title}</div>
+        <div className='text-xs'>{content}</div>
+    </div>
+  )
+}
