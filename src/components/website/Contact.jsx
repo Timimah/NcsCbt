9dae4e3361cdcd093cf5d3aa06f2559564e7e98c
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-10 19:33:36 +0100

    feat: create Contact component with contact information and message form

---
+++ src/components/website/Contact.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Button } from '../shared/Button';
+
+export const Contact = () => {
+    return (
+        <div className="mx-auto px-4">
+            <div className="text-center">
+                <h2 className="text-4xl text-primary mb-4">CONTACT INFORMATION</h2>
+                <p className="mb-8 text-grey">
+                    Feel free to reach out to us through various channels. We are available by phone, email, and social media for your convenience.
+                </p>
+            </div>
+
+            <div className="p-20 flex gap-10">
+                <div className='w-2/3'>
+                    <h3 className="text-4xl font-semibold text-primary mb-4">SEND ME A MESSAGE</h3>
+                    <p className="mb-6 text-grey">Have a specific inquiry or message for us? Please use the contact form below, and we'll get back to you promptly.</p>
+                </div>
+
+                <form className='w-full flex flex-col gap-4'>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <div>
+                            <label htmlFor="firstName" className="block mb-2">
+                                First Name
+                            </label>
+                            <input
+                                type="text"
+                                id="firstName"
+                                className="w-full py-2 px-3 border-b border-b-grey bg-transparent focus:border-b-primary focus:outline-none"
+                                placeholder="FIRST NAME"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="lastName" className="block mb-2">
+                                Last Name
+                            </label>
+                            <input
+                                type="text"
+                                id="lastName"
+                                className="w-full py-2 px-3 border-b border-b-grey bg-transparent focus:border-b-primary focus:outline-none"
+                                placeholder="LAST NAME"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <div>
+                            <label htmlFor="email" className="block mb-2">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="w-full py-2 px-3 border-b border-b-grey bg-transparent focus:border-b-primary focus:outline-none"
+                                placeholder="EMAIL ADDRESS"
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="phone" className="block mb-2">
+                                Phone Number
+                            </label>
+                            <input
+                                type="tel"
+                                id="phone"
+                                className="w-full py-2 px-3 border-b border-b-grey bg-transparent focus:border-b-primary focus:outline-none"
+                                placeholder="PHONE NUMBER"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mb-6">
+                        <label htmlFor="message" className="block mb-2">
+                            Your Message
+                        </label>
+                        <textarea
+                            id="message"
+                            className="w-full px-3 border-b border-b-grey bg-transparent focus:border-b-primary focus:outline-none"
+                            placeholder="MESSAGE"
+                        ></textarea>
+                    </div>
+
+                    <Button title="Send message" btnStyles="bg-primary w-fit hover:bg-green-600 text-white py-3 px-4 rounded-md" />
+                </form>
+            </div>
+        </div>
+    );
+};
