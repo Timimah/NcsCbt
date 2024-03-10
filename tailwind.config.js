37a677e81793ede362fc80f55abe364ff2654a3a
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-10 19:22:23 +0100

    feat: update color definitions in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
         primary: "#016f4a",
         secondary: "#e9fff7",
         grey: "#8A898D",
-        darkgrey: "464255"
+        darkgrey: "#797C86",
+        yellow: "#F8BA18",
+        cardgreen: "#016F4AB2",
+        story: "#D5FAFC",
+        footer: "#016F4A33"
       }
     },
   },
