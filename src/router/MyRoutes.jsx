4bc4d2bec0c63e3c1c2a93412c1f61c71dc67d81
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-13 13:01:28 +0100

    update MyRoutes

--- src/router/MyRoutes.jsx
+++ src/router/MyRoutes.jsx
@@ -8,6 +8,12 @@ import { Materials } from "../pages/UserPages/dashboard/Materials";
 import { Results } from "../pages/UserPages/dashboard/Results";
 import { ProfilePage } from "../pages/UserPages/dashboard/UserProfile";
 import { Home } from "../pages/WebsitePages/Home";
+import { AdminDashboard } from "../pages/AdminPages/dashboard/AdminDashboard";
+import { AdminLogin } from "../pages/AdminPages/auth/AdminLogin";
+import { AdminOverview } from "../pages/AdminPages/dashboard/AdminOverview";
+import { Checkin } from "../pages/AdminPages/dashboard/Checkin";
+import { User } from "../pages/AdminPages/dashboard/User";
+import { UploadMaterials } from "../pages/AdminPages/dashboard/UploadMaterials";
 
 export const MyRoutes = () => {
     return (
@@ -15,7 +21,7 @@ export const MyRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/take-exam" element={<TakeExam />} /> */}
+        <Route path="/login-admin" element={<AdminLogin />} />
         {/* <Route path="/search-results" element={<SearchResults />} />
         <Route path="/search-profile" element={<SearchProfilell />} /> */}
         <Route
@@ -29,6 +35,21 @@ export const MyRoutes = () => {
           {/* <Route path="practice" element={<DashboardUsers />} /> */}
           <Route path="result" element={<Results />} />
           <Route path="user-profile" element={<ProfilePage />} />
+        </Route>
+        <Route
+          path="/admin"
+          element={<AdminDashboard />}
+          // element={<Navigate replace to="/dashboard/overview" />}
+        >
+          <Route path="admin-overview" element={<AdminOverview />} />
+          <Route path="checkin" element={<Checkin />} />
+          <Route path="user" element={<User />} />
+          <Route path="materials" element={<UploadMaterials />} />
+          {/* <Route path="result" element={<Results />} />
+          <Route path="user-profile" element={<ProfilePage />} /> */}
+          {/* <Route path="/dashboard/add" element={<DashboardAdd />} />
+          <Route path="/dashboard/edit-venue" />
+          <Route path="/dashboard/edit-vendor" /> */}
           {/* <Route path="/dashboard/add" element={<DashboardAdd />} />
           <Route path="/dashboard/edit-venue" />
           <Route path="/dashboard/edit-vendor" /> */}
