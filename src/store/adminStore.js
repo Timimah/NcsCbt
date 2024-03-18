7ef4d66cf98e168e9e3552c5421a67d2c4189261
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-18 19:37:49 +0100

    feat: update store

--- src/store/adminStore.js
+++ src/store/adminStore.js
@@ -1,9 +1,51 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
-export const useAdminStore = create((set) => ({
-  // Admin state
+export const useAdminStore = create((set, get) => ({
+  admin: [],
+  adminIsLoggedIn: false,
+  loggedInAdmin: null,
+  subscriptions: [],
   quizzes: [],
 
+  init: () => {
+    const adminData = localStorage.getItem('adminData');
+    if (adminData) {
+      const parsedAdminData = JSON.parse(adminData);
+      set((state) => ({ ...state, admin: parsedAdminData, loggedInAdmin: parsedAdminData[0] }));
+    } else {
+      const Name = "Admin";
+      const dAdminEmail = 'admin@gmail.com';
+      const dAdminPassword = 'admin123';
+      const PhoneNumber = ""
+  
+      // Add the default admin object
+      const admin = [{ name: Name, adminEmail: dAdminEmail, adminPassword: dAdminPassword, phoneNumber: PhoneNumber }];
+      set((state) => ({ ...state, admin: admin, loggedInAdmin: null }));
+  
+      // Store the default admin data in local storage
+      localStorage.setItem('adminData', JSON.stringify(admin));
+    }
+  },
+
+  createAdmin: (adminData) => {
+    const newAdmin = [...get().admin, { ...adminData }];
+    set((state) => ({ ...state, admin: newAdmin }));
+    localStorage.setItem('adminData', JSON.stringify(newAdmin));
+  },
+
+  loginAdmin: (adminLoginData) => {
+    const { adminEmail, adminPassword } = adminLoginData;
+    const admin = get().admin.find((a) => a.adminEmail === adminEmail && a.adminPassword === adminPassword);
+    if (admin) {
+      set((state) => ({ ...state, adminIsLoggedIn: true, loggedInAdmin: admin }));
+      return true;
+    }
+    return false;
+  },
+
+  logoutAdmin: () => {
+    set((state) => ({ ...state, adminIsLoggedIn: false, loggedInAdmin: null }));
+  },
   // Actions
   addQuiz: (quiz) => set((state) => ({ quizzes: [...state.quizzes, quiz] })),
   updateQuiz: (updatedQuiz) =>
@@ -16,6 +58,10 @@ export const useAdminStore = create((set) => ({
     set((state) => ({
       quizzes: state.quizzes.filter((quiz) => quiz.id !== quizId),
     })),
+
+    materials: [],
+  addMaterial: (material) =>
+    set((state) => ({ materials: [...state.materials, material] })),
 
   // Other admin-related state and actions
   users: [],
@@ -31,3 +77,5 @@ export const useAdminStore = create((set) => ({
       users: state.users.filter((user) => user.id !== userId),
     })),
 }));
+
+useAdminStore.getState().init();
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,19 +1,63 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
-export const useUserStore = create((set) => ({
-  // User state
-  user: null,
+export const useUserStore = create((set, get) => ({
+  users: [],
   isLoggedIn: false,
-
-  // Actions
-  setUser: (userData) => set({ user: userData, isLoggedIn: true }),
-  logout: () => set({ user: null, isLoggedIn: false }),
-
-  // Other user-related state and actions
+  loggedInUser: null,
+  subscriptions: [],
   quizzes: [],
-  addQuiz: (quiz) => set((state) => ({ quizzes: [...state.quizzes, quiz] })),
-  removeQuiz: (quizId) =>
+
+  init: () => {
+    const userData = localStorage.getItem('userData');
+    if (userData) {
+      set((state) => ({ ...state, users: JSON.parse(userData), loggedInUser: null }));
+    } else {
+      set((state) => ({ ...state, loggedInUser: null }));
+    }
+  },
+
+  createUser: (userData) => {
+    const newUsers = [...get().users, { ...userData }];
+    set((state) => ({ ...state, users: newUsers }));
+    localStorage.setItem('userData', JSON.stringify(newUsers));
+  },
+
+  loginUser: (loginData) => {
+    const { examineeId, password } = loginData;
+    const user = get().users.find((user) => user.id === examineeId && user.password === password);
+    if (user) {
+      set((state) => ({ ...state, isLoggedIn: true, loggedInUser: user }));
+      return true;
+    }
+    return false;
+  },
+
+  logoutUser: () => {
+    set((state) => ({ ...state, isLoggedIn: false, loggedInUser: null }));
+  },
+
+  setSubscription: (subscriptionData) => {
+    set((state) => ({ ...state, subscriptions: [...state.subscriptions, subscriptionData] }));
+  },
+
+  cancelSubscription: (subscriptionId) => {
     set((state) => ({
+      ...state,
+      subscriptions: state.subscriptions.filter((subscription) => subscription.id !== subscriptionId),
+    }));
+  },
+
+  addQuiz: (quiz) => {
+    set((state) => ({ ...state, quizzes: [...state.quizzes, quiz] }));
+  },
+
+  removeQuiz: (quizId) => {
+    set((state) => ({
+      ...state,
       quizzes: state.quizzes.filter((quiz) => quiz.id !== quizId),
-    })),
+    }));
+  }
 }));
+
+// Initialize the store
+useUserStore.getState().init();
