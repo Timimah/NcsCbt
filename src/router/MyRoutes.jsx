3e2b9f3f66656120a70caaa52a20df3955b17aca
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-18 19:38:19 +0100

    update routes

--- src/router/MyRoutes.jsx
+++ src/router/MyRoutes.jsx
@@ -10,10 +10,12 @@ import { ProfilePage } from "../pages/UserPages/dashboard/UserProfile";
 import { Home } from "../pages/WebsitePages/Home";
 import { AdminDashboard } from "../pages/AdminPages/dashboard/AdminDashboard";
 import { AdminLogin } from "../pages/AdminPages/auth/AdminLogin";
-import { AdminOverview } from "../pages/AdminPages/dashboard/AdminOverview";
 import { Checkin } from "../pages/AdminPages/dashboard/Checkin";
 import { User } from "../pages/AdminPages/dashboard/User";
 import { UploadMaterials } from "../pages/AdminPages/dashboard/UploadMaterials";
+import { Payment } from "../pages/AdminPages/dashboard/Payment";
+import { Result } from "../pages/AdminPages/dashboard/Result";
+import { AdminOverview } from "../pages/AdminPages/dashboard/AdminOverview";
 
 export const MyRoutes = () => {
     return (
@@ -21,13 +23,12 @@ export const MyRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/login-admin" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminLogin />} />
         {/* <Route path="/search-results" element={<SearchResults />} />
         <Route path="/search-profile" element={<SearchProfilell />} /> */}
         <Route
           path="/dashboard"
           element={<Dashboard />}
-          // element={<Navigate replace to="/dashboard/overview" />}
         >
           <Route path="overview" element={<DashboardPage />} />
           <Route path="take-exam" element={<TakeExam />} />
@@ -37,16 +38,15 @@ export const MyRoutes = () => {
           <Route path="user-profile" element={<ProfilePage />} />
         </Route>
         <Route
-          path="/admin"
+          path="/admin-dashboard"
           element={<AdminDashboard />}
-          // element={<Navigate replace to="/dashboard/overview" />}
         >
-          <Route path="admin-overview" element={<AdminOverview />} />
+          <Route path="overview" element={<AdminOverview />} />
           <Route path="checkin" element={<Checkin />} />
           <Route path="user" element={<User />} />
           <Route path="materials" element={<UploadMaterials />} />
-          {/* <Route path="result" element={<Results />} />
-          <Route path="user-profile" element={<ProfilePage />} /> */}
+          <Route path="result" element={<Result />} />
+          <Route path="payment-history" element={<Payment />} />
           {/* <Route path="/dashboard/add" element={<DashboardAdd />} />
           <Route path="/dashboard/edit-venue" />
           <Route path="/dashboard/edit-vendor" /> */}
