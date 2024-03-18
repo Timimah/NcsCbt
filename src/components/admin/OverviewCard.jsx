8d9cf6cd2fd98844d292b8deb5d00b6b400e7c4f
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-18 19:39:17 +0100

    create overviewcard component for admin dashboard

---
+++ src/components/admin/OverviewCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const OverviewCard = ({ cardStyles, label, icon, cardValue, cardClick }) => {
+    return (
+        <div
+            className={`bg-white rounded-lg shadow-md flex items-center cursor-pointer w-full gap-4 ${cardStyles}`}
+            onClick={cardClick}
+        >
+            <div className=''>
+                {icon}
+            </div>
+            <div>
+                <div className="font-bold text-2xl">{cardValue}</div>
+                <div className="text-xs font-light" >{label}</div>
+            </div>
+        </div>
+    )
+}
