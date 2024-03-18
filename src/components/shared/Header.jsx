8ae81aaa73c0a03e28253f7cd8ea7819cd9c6dac
Timimah <44163985+Timimah@users.noreply.github.com>
2024-03-18 19:38:45 +0100

    move header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useAdminStore } from '../../store/adminStore';
 
-export const Header = ({ title, username }) => {
+export const Header = ({ title }) => {
     const navigate = useNavigate();
+    const { loggedInAdmin } = useAdminStore();
+    console.log(loggedInAdmin);
+    const userName = loggedInAdmin ? loggedInAdmin.name : 'Admin';
+
 
     const handleProfile = () => {
         navigate('/dashboard/user-profile')
@@ -12,13 +17,12 @@ export const Header = ({ title, username }) => {
         <header className="py-4 px-6 flex justify-between items-center w-full  ">
             <div className='flex flex-col gap-2'>
                 <h1 className="text-3xl font-bold text-darkgrey">{title}</h1>
-                <p className="text-sm text-grey">Welcome, <span>{username}</span></p>
+                <p className="text-sm text-grey">Welcome, <span>{userName}</span></p>
             </div>
             <div className="flex items-center cursor-pointer" onClick={handleProfile}>
                 <img alt="User Avatar" className="w-8 h-8 rounded-full mr-2" />
                 <div>
-                    <span className='text-grey'>Hello,</span>
-                    <span className='font-bold text-darkgrey'>{username}</span>
+                    <span className='font-bold text-darkgrey'>{userName}</span>
                 </div>
             </div>
         </header>
