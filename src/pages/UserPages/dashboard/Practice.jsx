c65d15853bf1d08c87d189b224545fa5d5e81c09
Timimah <44163985+Timimah@users.noreply.github.com>
2024-04-07 14:53:52 +0100

    create practice page

---
+++ src/pages/UserPages/dashboard/Practice.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { Modal } from '../../../components/shared/Modal';
+import { Button } from '../../../components/shared/Button';
+import { Header } from '../../../components/shared/Header';
+
+export const Practice = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [rank, setRank] = useState("");
+    const [time, setTime] = useState("5 min");
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        console.log('Practice Page')
+        setShowModal(true);
+    }
+        , []);
+    return (
+        <div className='flex items-center justify-center px-8'>
+            <Header title="Practice" />
+            {showModal && (
+                <Modal
+                    title="Quiz Settings"
+                    closeModal={() => setShowModal(false)}
+                    modStyles="bg-secondary w-1/2 h-fit"
+                    content={
+                        <div className='my-10'>
+                            <div>
+                                <label htmlFor="time">Set Time</label>
+                                <input
+                                    type="text"
+                                    id="time"
+                                    value={time}
+                                    className="border w-full py-4 px-4 rounded-lg shadow-sm text-sm hover:border-primary"
+                                    placeholder="5 min"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="rank" className="block py-1 -mb-1">Select Rank</label>
+                                <select id="rank" name="rank" value={rank} onChange={(e) => { setRank(e.target.value); setError("") }} className={`border w-full py-4 px-4 rounded-lg shadow-sm text-sm hover:border-primary ${error ? 'border-red-500' : ''}`}>
+                                    <option value="">Select a category</option>
+                                    <option value="CAI">CAI</option>
+                                    <option value="CAII">CAII</option>
+                                    <option value="AIC">AIC</option>
+                                    <option value="IC">IC</option>
+                                    <option value="ASCII">ASCII</option>
+                                    <option value="ASCI">ASCI</option>
+                                    <option value="DSC">DSC</option>
+                                    <option value="SC">SC</option>
+                                    <option value="CSC">CSC</option>
+                                    <option value="AC">AC</option>
+                                    <option value="DC">DC</option>
+                                    <option value="CC">CC</option>
+                                </select>
+                                {error && <div className="text-sm text-red-500">{error}</div>}
+                            </div>
+                        </div>
+                    }
+                    buttons={
+                        <Button
+                            title="Start Quiz"
+                            btnStyles="bg-primary text-white py-2 px-4 rounded-md mt-4 w-full"
+                        />
+                    }
+                />
+            )}
+        </div>
+    )
+}
