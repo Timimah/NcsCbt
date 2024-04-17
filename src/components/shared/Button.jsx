044954672a502e778c02c803977753ec6d997f84
Blessed-Agboola Jesujoba J. <44163985+Timimah@users.noreply.github.com>
2024-04-17 21:42:49 +0000

    add disabled attribute

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Button = ({ title, btnStyles, btnClick, type }) => {
+export const Button = ({ title, btnStyles, btnClick, type, disable }) => {
     return (
         <>
-            <button type={type} className={`relative text-center flex justify-center items-center ${btnStyles}`} onClick={btnClick}>
+            <button type={type} className={`relative text-center flex justify-center items-center ${btnStyles}`} onClick={btnClick} disabled={disable}>
                 <span className="relative">{title}</span>
             </button>
         </>
