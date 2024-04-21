85d059a808ee786d171739856e13e9a86c00cb8e
Blessed-Agboola Jesujoba J. <44163985+Timimah@users.noreply.github.com>
2024-04-21 14:29:00 +0000

    update package, firebase init

---
+++ config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpXPTw_r0dS2TXsddLYXJIFFVZGXki2rE",
+  authDomain: "ncs-prep.firebaseapp.com",
+  projectId: "ncs-prep",
+  storageBucket: "ncs-prep.appspot.com",
+  messagingSenderId: "122676239504",
+  appId: "1:122676239504:web:6faa47f0c9d39de8428c15",
+  measurementId: "G-BZM10J6BBH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const materialsStorage = getStorage(app);
+export const imageStorage = getStorage(app);
