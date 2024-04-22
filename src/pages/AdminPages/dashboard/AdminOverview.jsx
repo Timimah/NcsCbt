cd1902480043539c31b494580cce6f007b16a5ea
excellence-arch <oladipupomichael9@gmail.com>
2024-04-22 20:24:02 +0100

    get practice questions now working

--- src/pages/AdminPages/dashboard/AdminOverview.jsx
+++ src/pages/AdminPages/dashboard/AdminOverview.jsx
@@ -1,145 +1,155 @@
-import React, { useEffect, useState } from 'react';
-import { Header } from '../../../components/shared/Header';
-import { materials } from '../../../components/user/materials';
-import { Button } from '../../../components/shared/Button';
-import { Modal } from '../../../components/shared/Modal';
-import { Chart } from '../../../components/user/Chart';
-import user from '../../../assets/user.png'
-import adminn from '../../../assets/user.png'
-import subscription from '../../../assets/sub.png'
-import examinee from '../../../assets/examinee.png'
-import { OverviewCard } from '../../../components/admin/OverviewCard';
-import { useUserStore } from '../../../store/userStore';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { Header } from "../../../components/shared/Header";
+import { materials } from "../../../components/user/materials";
+import { Button } from "../../../components/shared/Button";
+import { Modal } from "../../../components/shared/Modal";
+import { Chart } from "../../../components/user/Chart";
+import user from "../../../assets/user.png";
+import adminn from "../../../assets/user.png";
+import subscription from "../../../assets/sub.png";
+import examinee from "../../../assets/examinee.png";
+import { OverviewCard } from "../../../components/admin/OverviewCard";
+import { useUserStore } from "../../../store/userStore";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const AdminOverview = () => {
-    const { users, materials, questions, setUsers, setMaterials, setQuestions, isLoggedIn } = useUserStore();
-    const navigate = useNavigate();
-    const [showModal, setShowModal] = useState(false);
+  const {
+    users,
+    materials,
+    questions,
+    setUsers,
+    setMaterials,
+    setQuestions,
+    isLoggedIn,
+  } = useUserStore();
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
-    const cards = [
-        {
-            icon: user,
-            value: users.length,
-            label: 'Total User',
-            bgColor: 'bg-green-100',
-            textColor: 'text-green-600',
-            iconBgColor: 'bg-green-500',
-            iconTextColor: 'text-white',
-        },
-        {
-            icon: adminn,
-            value: 0,
-            label: 'Total Admin',
-            bgColor: 'bg-blue-100',
-            textColor: 'text-blue-600',
-            iconBgColor: 'bg-blue-500',
-            iconTextColor: 'text-white',
-        },
-        {
-            icon: subscription,
-            value: 0,
-            label: 'Total Subscriptions',
-            bgColor: 'bg-green-100',
-            textColor: 'text-green-600',
-            iconBgColor: 'bg-green-500',
-            iconTextColor: 'text-white',
-        },
-        {
-            icon: examinee,
-            value: 0,
-            label: 'Total Examinee',
-            bgColor: 'bg-blue-100',
-            textColor: 'text-blue-600',
-            iconBgColor: 'bg-blue-500',
-            iconTextColor: 'text-white',
-        }
-    ]
+  const cards = [
+    {
+      icon: user,
+      value: users.length,
+      label: "Total User",
+      bgColor: "bg-green-100",
+      textColor: "text-green-600",
+      iconBgColor: "bg-green-500",
+      iconTextColor: "text-white",
+    },
+    {
+      icon: adminn,
+      value: 0,
+      label: "Total Admin",
+      bgColor: "bg-blue-100",
+      textColor: "text-blue-600",
+      iconBgColor: "bg-blue-500",
+      iconTextColor: "text-white",
+    },
+    {
+      icon: subscription,
+      value: 0,
+      label: "Total Subscriptions",
+      bgColor: "bg-green-100",
+      textColor: "text-green-600",
+      iconBgColor: "bg-green-500",
+      iconTextColor: "text-white",
+    },
+    {
+      icon: examinee,
+      value: 0,
+      label: "Total Examinee",
+      bgColor: "bg-blue-100",
+      textColor: "text-blue-600",
+      iconBgColor: "bg-blue-500",
+      iconTextColor: "text-white",
+    },
+  ];
 
-    useEffect(() => {
-        const token = localStorage.getItem("auth-token")
-        if (isLoggedIn) {
-            axios.get("https://ncs-cbt-api.onrender.com/admin/getUsers", {
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
+  useEffect(() => {
+    const token = localStorage.getItem("auth-token");
+    if (isLoggedIn) {
+      axios
+        .get("https://ncs-cbt-api.onrender.com/admin/getUsers", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        })
+        .then((res) => {
+          setUsers(res.data.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      axios
+        .get("https://ncs-cbt-api.onrender.com/material/", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          setMaterials(res.data.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      axios
+        .get("https://ncs-cbt-api.onrender.com/exam/", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          // console.log(res.data)
+          setQuestions(res.data.data);
+          // console.log(materials)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      navigate("/admin");
+    }
+  }, []);
+  console.log(users, materials, questions);
+
+  const sortedMaterials = materials.sort((a, b) => b.rating - a.rating);
+
+  // Get the top 4 materials with the highest rating
+  const topMaterials = sortedMaterials.slice(0, 4);
+
+  return (
+    <div className="flex flex-col w-full p-10">
+      <Header title="Dashboard" />
+      <main className="flex-grow">
+        <div className="flex justify-around gap-6 p-5">
+          {cards.map((card, index) => (
+            <div key={index} className="w-fit">
+              <OverviewCard
+                cardStyles="p-6"
+                label={card.label}
+                cardValue={card.value}
+                icon={
+                  <>
+                    <img className="w-10" src={card.icon} alt={card.icon} />
+                  </>
                 }
-            })
-                .then((res) => {
-                    setUsers(res.data.data)
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-
-            axios.get("https://ncs-cbt-api.onrender.com/material/", {
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
-            })
-                .then((res) => {
-                    console.log(res)
-                    setMaterials(res.data.data)
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-
-            axios.get("https://ncs-cbt-api.onrender.com/exam/", {
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
-            })
-                .then((res) => {
-                    // console.log(res.data)
-                    setQuestions(res.data.data)
-                    // console.log(materials)
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-        } else {
-            navigate('/admin')
-        }
-    }, []);
-    console.log(users, materials, questions);
-
-    const sortedMaterials = materials.sort((a, b) => b.rating - a.rating);
-
-    // Get the top 4 materials with the highest rating
-    const topMaterials = sortedMaterials.slice(0, 4);
-
-    return (
-        <div className="flex flex-col w-full p-10">
-            <Header title="Dashboard" />
-            <main className="flex-grow">
-                <div className="flex justify-around gap-6 p-5">
-                    {cards.map((card, index) => (
-                        <div key={index} className='w-fit'>
-                            <OverviewCard
-                                cardStyles="p-6"
-                                label={card.label}
-                                cardValue={card.value}
-                                icon={
-                                    <>
-                                        <img className='w-10' src={card.icon} alt={card.icon} />
-                                    </>
-                                }
-                            />
-                        </div>
-                    ))}
-                </div>
-
-                <section className="mt-8">
-                    <h2 className="text-xl font-bold mb-4">Test Overview</h2>
-                    <div className='bg-white'>
-                        <Chart />
-                    </div>
-                </section>
-            </main>
+              />
+            </div>
+          ))}
         </div>
-    );
+
+        <section className="mt-8">
+          <h2 className="text-xl font-bold mb-4">Test Overview</h2>
+          <div className="bg-white">
+            <Chart />
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
--- src/pages/UserPages/dashboard/Practice.jsx
+++ src/pages/UserPages/dashboard/Practice.jsx
@@ -6,222 +6,234 @@ import axios from "axios";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export const Practice = () => {
-	const navigate = useNavigate();
-	const [showModal, setShowModal] = useState(false);
-    const [showInstructions, setShowInstructions] = useState(false);
-	const [rank, setRank] = useState("");
-	const [time, setTime] = useState(60);
-	const [error, setError] = useState("");
-	const [allQuestions, setAllQuestions] = useState(
-		JSON.parse(localStorage.getItem("questions")) || []
-	);
-	const token = localStorage.getItem("auth-token");
-	let practiceQuestions;
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const [showInstructions, setShowInstructions] = useState(false);
+  const [rank, setRank] = useState("");
+  const [time, setTime] = useState(60);
+  const [error, setError] = useState("");
+  const [allQuestions, setAllQuestions] = useState(
+    JSON.parse(localStorage.getItem("questions")) || []
+  );
+  const token = localStorage.getItem("auth-token");
+  let practiceQuestions;
 
-	const getQuestions = () => {
-		if (allQuestions.length > 0) {
-			practiceQuestions = allQuestions.questionDetails.filter(
-				(question) => question.category === rank
-			);
-			console.log(practiceQuestions);
-		} else {
-			practiceQuestions = [];
-		}
-		localStorage.setItem("practiceQuestions", JSON.stringify(practiceQuestions));
-	};
+  const getQuestions = () => {
+    if (allQuestions.length > 0) {
+      practiceQuestions = allQuestions.questionDetails.filter(
+        (question) => question.category === rank
+      );
+      console.log(practiceQuestions);
+    } else {
+      practiceQuestions = [];
+    }
+    localStorage.setItem(
+      "practiceQuestions",
+      JSON.stringify(practiceQuestions)
+    );
+  };
 
-	useEffect(() => {
-		console.log("Practice Page");
-		setShowModal(true);
-		getQuestions();
-	}, []);
+  useEffect(() => {
+    console.log("Practice Page");
+    setShowModal(true);
+    getQuestions();
+  }, []);
 
-	const getPracticeQuestions = async () => {
-		if (time === "") {
-			setError("Please select a time");
-		}
-		if (rank === "") {
-			setError("Please select a category");
-		} else {
-			getQuestions();
-			localStorage.setItem("practiceQuestionsDetails", JSON.stringify({ rank, time }));
-			// navigate("/take-exam");
-			await axios.get(
-					"https://ncs-cbt-api.onrender.com/exam/getPracticeQuestions",
-					{ rank },
-					{
-						headers: {
-							"Authorization": `Bearer ${token}`,
-						},
-					}
-				)
-				.then((res) => {
-					console.log(res);
-                    setShowModal(false);
-                    setShowInstructions(true);
-					// setQuestions(res.data.data || []);
-					// setDisplayedQuestions(questions);
-					// const questionsForSelectedCategory =
-					//     questions.filter(
-					//         (question) =>
-					//             question.category === selectedCategory
-					//     );
-					// console.log(
-					//     questions,
-					//     questionsForSelectedCategory
-					// );
-					// setEditQuestions(
-					//     questionsForSelectedCategory
-					// );
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	};
-	return (
-		<div className='flex items-center justify-center px-8'>
-			<Header title='Practice' />
-			{showModal && (
-				<Modal
-					title='Quiz Settings'
-					closeModal={() => setShowModal(false)}
-					modStyles='bg-secondary md:w-1/2 h-fit'
-					content={
-						<div className='my-10'>
-							<div>
-								{error && <div className='text-sm text-red-500'>{error}</div>}
-								<label htmlFor='time'>Set Time (minutes)</label>
-								<input
-									type='tel'
-									id='time'
-									value={time}
-									className='border w-full py-4 px-4 rounded-lg shadow-sm text-sm hover:border-primary'
-									placeholder='5 min'
-									onChange={(e) => setTime(e.target.value)}
-								/>
-							</div>
-							<div>
-								<label
-									htmlFor='rank'
-									className='block py-1 -mb-1'>
-									Select Rank
-								</label>
-								<select
-									id='rank'
-									name='rank'
-									value={rank}
-									onChange={(e) => {
-										setRank(e.target.value);
-										setError("");
-									}}
-									className={`border w-full py-4 px-4 rounded-lg shadow-sm text-sm hover:border-primary ${
-										error ? "border-red-500" : ""
-									}`}>
-									<option value=''>Select a category</option>
-									<option value='CAI-CAII'>CAI-CAII</option>
-									<option value='CAII-AIC'>CAII-AIC</option>
-									<option value='AIC-IC'>AIC-IC</option>
-									<option value='IC-ASCII'>IC-ASCII</option>
-									<option value='ASCII-ASCI'>ASCII-ASCI</option>
-									<option value='ASCI-DSC'>ASCI-DSC</option>
-									<option value='DSC-SC'>DSC-SC</option>
-									<option value='SC-CSC'>SC-CSC</option>
-									<option value='CSC-AC'>CSC-AC</option>
-									<option value='AC-DC'>AC-DC</option>
-									<option value='DC-CC'>DC-CC</option>
-								</select>
-							</div>
-						</div>
-					}
-					buttons={
-						<Button
-							title='Start Quiz'
-							btnStyles='bg-primary text-white py-2 px-4 rounded-md mt-4 w-full'
-							btnClick={getPracticeQuestions}
-						/>
-					}
-				/>
-			)}
-			{showInstructions && (
-				<Modal
-					title='Instructions'
-					closeModal={() => setShowInstructions(false)}
-					modStyles='bg-secondary w-2/3 h-2/3 my-10 overflow-y-scroll'
-					content={
-							<div className='rounded-lg p-8 mx-auto relative'>
-								<p className='mb-4'>
-									Welcome to the NCS CBT platform. Please read the following
-									instructions carefully before starting the exam.
-								</p>
-								<p className='mb-4'>
-									Before proceeding, ensure that your computer and internet connection are
-									working properly. Click on the 'System Check' button to verify.
-								</p>
-								<ol className='list-decimal list-inside mb-4'>
-									<li>
-										<strong>Time Reminder:</strong>{" "}
-										<span className='font-normal'>
-											You will have {time} minutes to complete the exam. A timer will be
-											displayed on the screen to help you manage your time effectively.
-										</span>
-									</li>
-									<li>
-										<strong>Navigation Instructions:</strong>{" "}
-										<span className='font-normal'>
-											Use the navigation buttons provided to move between questions. You
-											can go back and forth as needed.
-										</span>
-									</li>
-									<li>
-										<strong>Answering Format:</strong>{" "}
-										<span className='font-normal'>
-											Answer each question by selecting the appropriate option.
-										</span>
-									</li>
-									<li>
-										<strong>Marking for Review:</strong>{" "}
-										<span className='font-normal'>
-											If you're unsure about an answer, you can mark the question for
-											review and return to it later.
-										</span>
-									</li>
-									<li>
-										<strong>Technical Support Information:</strong>{" "}
-										<span className='font-normal'>
-											If you encounter any technical issues during the exam, please raise
-											your hand or contact the invigilator for assistance.
-										</span>
-									</li>
-									<li>
-										<strong>Academic Integrity Reminder:</strong>{" "}
-										<span className='font-normal'>
-											Maintain academic integrity throughout the exam. Any form of
-											cheating or unauthorized assistance is strictly prohibited.
-										</span>
-									</li>
-									<li>
-										<strong>Exam Duration Notice:</strong>{" "}
-										<span className='font-normal'>
-											Once the exam starts, it cannot be paused or extended. Ensure that
-											you manage your time effectively to complete all questions.
-										</span>
-									</li>
-								</ol>
-						</div>
-					}
-                    buttons={
-                        <Button
-                            title="Start Exam"
-                            btnStyles="bg-primary text-white py-2 px-4 rounded-md mt-4 w-fit mx-auto"
-                            btnClick={() => {
-                                setShowInstructions(false);
-                                navigate("/take-exam");
-                            }}
-                        />
-                    }
-				/>
-			)}
-		</div>
-	);
+  const getPracticeQuestions = async () => {
+    if (!time) {
+      setError("Please select a time");
+    }
+    if (!rank) {
+      setError("Please select a category");
+    } else {
+      getQuestions();
+      localStorage.setItem(
+        "practiceQuestionsDetails",
+        JSON.stringify({ rank, time })
+      );
+      console.log(rank);
+      // navigate("/take-exam");
+
+      axios
+        .post(
+          "https://ncs-cbt-api.onrender.com/exam/getPracticeQuestions",
+          { rank },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          setShowModal(false);
+          setShowInstructions(true);
+          // setQuestions(res.data.data || []);
+          // setDisplayedQuestions(questions);
+          // const questionsForSelectedCategory =
+          //     questions.filter(
+          //         (question) =>
+          //             question.category === selectedCategory
+          //     );
+          // console.log(
+          //     questions,
+          //     questionsForSelectedCategory
+          // );
+          // setEditQuestions(
+          //     questionsForSelectedCategory
+          // );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+  return (
+    <div className="flex items-center justify-center px-8">
+      <Header title="Practice" />
+      {showModal && (
+        <Modal
+          title="Quiz Settings"
+          closeModal={() => setShowModal(false)}
+          modStyles="bg-secondary md:w-1/2 h-fit"
+          content={
+            <div className="my-10">
+              <div>
+                {error && <div className="text-sm text-red-500">{error}</div>}
+                <label htmlFor="time">Set Time (minutes)</label>
+                <input
+                  type="tel"
+                  id="time"
+                  value={time}
+                  className="border w-full py-4 px-4 rounded-lg shadow-sm text-sm hover:border-primary"
+                  placeholder="5 min"
+                  onChange={(e) => setTime(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="rank" className="block py-1 -mb-1">
+                  Select Rank
+                </label>
+                <select
+                  id="rank"
+                  name="rank"
+                  value={rank}
+                  onChange={(e) => {
+                    setRank(e.target.value);
+                    setError("");
+                  }}
+                  className={`border w-full py-4 px-4 rounded-lg shadow-sm text-sm hover:border-primary ${
+                    error ? "border-red-500" : ""
+                  }`}
+                >
+                  <option value="">Select a category</option>
+                  <option value="CAI-CAII">CAI-CAII</option>
+                  <option value="CAII-AIC">CAII-AIC</option>
+                  <option value="AIC-IC">AIC-IC</option>
+                  <option value="IC-ASCII">IC-ASCII</option>
+                  <option value="ASCII-ASCI">ASCII-ASCI</option>
+                  <option value="ASCI-DSC">ASCI-DSC</option>
+                  <option value="DSC-SC">DSC-SC</option>
+                  <option value="SC-CSC">SC-CSC</option>
+                  <option value="CSC-AC">CSC-AC</option>
+                  <option value="AC-DC">AC-DC</option>
+                  <option value="DC-CC">DC-CC</option>
+                </select>
+              </div>
+            </div>
+          }
+          buttons={
+            <Button
+              title="Start Quiz"
+              btnStyles="bg-primary text-white py-2 px-4 rounded-md mt-4 w-full"
+              btnClick={getPracticeQuestions}
+            />
+          }
+        />
+      )}
+      {showInstructions && (
+        <Modal
+          title="Instructions"
+          closeModal={() => setShowInstructions(false)}
+          modStyles="bg-secondary w-2/3 h-2/3 my-10 overflow-y-scroll"
+          content={
+            <div className="rounded-lg p-8 mx-auto relative">
+              <p className="mb-4">
+                Welcome to the NCS CBT platform. Please read the following
+                instructions carefully before starting the exam.
+              </p>
+              <p className="mb-4">
+                Before proceeding, ensure that your computer and internet
+                connection are working properly. Click on the 'System Check'
+                button to verify.
+              </p>
+              <ol className="list-decimal list-inside mb-4">
+                <li>
+                  <strong>Time Reminder:</strong>{" "}
+                  <span className="font-normal">
+                    You will have {time} minutes to complete the exam. A timer
+                    will be displayed on the screen to help you manage your time
+                    effectively.
+                  </span>
+                </li>
+                <li>
+                  <strong>Navigation Instructions:</strong>{" "}
+                  <span className="font-normal">
+                    Use the navigation buttons provided to move between
+                    questions. You can go back and forth as needed.
+                  </span>
+                </li>
+                <li>
+                  <strong>Answering Format:</strong>{" "}
+                  <span className="font-normal">
+                    Answer each question by selecting the appropriate option.
+                  </span>
+                </li>
+                <li>
+                  <strong>Marking for Review:</strong>{" "}
+                  <span className="font-normal">
+                    If you're unsure about an answer, you can mark the question
+                    for review and return to it later.
+                  </span>
+                </li>
+                <li>
+                  <strong>Technical Support Information:</strong>{" "}
+                  <span className="font-normal">
+                    If you encounter any technical issues during the exam,
+                    please raise your hand or contact the invigilator for
+                    assistance.
+                  </span>
+                </li>
+                <li>
+                  <strong>Academic Integrity Reminder:</strong>{" "}
+                  <span className="font-normal">
+                    Maintain academic integrity throughout the exam. Any form of
+                    cheating or unauthorized assistance is strictly prohibited.
+                  </span>
+                </li>
+                <li>
+                  <strong>Exam Duration Notice:</strong>{" "}
+                  <span className="font-normal">
+                    Once the exam starts, it cannot be paused or extended.
+                    Ensure that you manage your time effectively to complete all
+                    questions.
+                  </span>
+                </li>
+              </ol>
+            </div>
+          }
+          buttons={
+            <Button
+              title="Start Exam"
+              btnStyles="bg-primary text-white py-2 px-4 rounded-md mt-4 w-fit mx-auto"
+              btnClick={() => {
+                setShowInstructions(false);
+                navigate("/take-exam");
+              }}
+            />
+          }
+        />
+      )}
+    </div>
+  );
 };
