19e485efd7e2194ded16640ba8207f6969d6319b
Timimah <44163985+Timimah@users.noreply.github.com>
2024-05-10 14:43:49 +0100

    display questions based on type

--- src/pages/UserPages/dashboard/TakeExam.jsx
+++ src/pages/UserPages/dashboard/TakeExam.jsx
@@ -18,18 +18,25 @@ export const TakeExam = () => {
   const [answered, setAnswered] = useState(false);
   const [answers, setAnswers] = useState([]);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
-  const [timeRemaining, setTimeRemaining] = useState(); // 10 minutes in seconds
+  const [timeRemaining, setTimeRemaining] = useState();
+  // const [duration, setDuration] = useState();
   const [showModal, setShowModal] = useState(false);
   const [resultModal, setResultModal] = useState(false);
   const [score, setScore] = useState("");
   const [reviewing, setReviewing] = useState(false);
   const [answer, setAnswer] = useState();
+
   let timer;
+  let quizDetails;
+  const quizType = localStorage.getItem("type");
+  // console.log(quizType)
 
   useEffect(() => {
-    const quizDetails = JSON.parse(
-      localStorage.getItem("practiceQuestionsDetails"),
-    );
+    if (quizType === "practice") {
+      quizDetails = JSON.parse(localStorage.getItem("practiceQuestionsDetails"));
+    } else {
+      quizDetails = JSON.parse(localStorage.getItem("examQuestionsDetails"));
+    }
     console.log(quizDetails);
     // setAnswers([]);
     setTimeRemaining(quizDetails.time * 60);
@@ -145,9 +152,12 @@ export const TakeExam = () => {
                 window.history.back();
               }
             }}
+            disable={quizType === "exam"}
           />
         </div>
-        <Header title="Quiz" />
+        {
+          quizType === "practice" ? <Header title="Practice" /> : <Header title="Exam" />
+        }
       </div>
       {reviewing === true && (
         <div className="absolute top-32 bottom-0 left-0 right-0 z-20 bg-white p-10 flex flex-col overflow-y-scroll">
@@ -173,31 +183,30 @@ export const TakeExam = () => {
           </div>
         </div>
       )}
-      <div className="flex gap-8 justify-between items-center h-full w-full px-10">
-        <div className="w-1/2 px-8 flex flex-col h-full gap-8 self-start">
+      <div className="flex gap-8 justify-between items-center w-full px-10">
+        <div className="md:w-1/2 md:px-8 px-4 flex flex-col mb-10 gap-8">
           <div className="flex flex-col gap-8">
             <div className="flex justify-between items-center">
-              <div className="text-lg">Question {currentQuestion + 1}</div>
+              <div className="text-3xl">Question {currentQuestion + 1}</div>
               <div
-                className={`text-3xl font-bold ${timeRemaining <= 300 ? "animate-pulse text-red-600 bg-transparent" : "text-white bg-red-600"}`}
+                className={`text-3xl font-bold px-4 py-3 ${timeRemaining <= 300 ? "animate-pulse text-red-600 bg-transparent" : "text-white bg-red-600"}`}
               >
                 {Math.floor(timeRemaining / 60)}:
                 {(timeRemaining % 60).toString().padStart(2, "0")}
               </div>
             </div>
-            <div className="font-bold text-lg">
+            <div className="font-semibold text-lg">
               {questions[currentQuestion].question}
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-6 mb-8">
+          <div className="flex flex-col md:grid md:grid-cols-2 gap-6 mb-8">
             {questions[currentQuestion].options.map((option, index) => (
               <div
                 key={index}
-                className={`flex items-center justify-center rounded-lg px-4 py-3 cursor-pointer border border-primary text-primary text-lg font-bold  ${
-                  answers[currentQuestion] === option
-                    ? "bg-primary text-white"
-                    : ""
-                }`}
+                className={`flex items-center justify-center rounded-lg px-4 py-3 cursor-pointer border border-primary text-primary font-bold  ${answers[currentQuestion] === option
+                  ? "bg-primary text-white"
+                  : ""
+                  }`}
                 onClick={() =>
                   handleOptionChange(option, questions[currentQuestion].id)
                 }
@@ -209,56 +218,53 @@ export const TakeExam = () => {
               </div>
             ))}
           </div>
-          <div className="flex justify-evenly gap-8" id="navButton">
+          <div className="flex md:flex-row flex-col md:justify-evenly gap-8" id="navButton">
             <Button
               title={
-                <div className="flex gap-1 items-center">
+                <div className="flex px-4 gap-1 items-center">
                   <img src={prev} className="" alt="" />
                   <span>Previous</span>
                 </div>
               }
-              btnStyles={`text-lg bg-secondary shadow-md px-4 py-3 rounded-lg w-full ${
-                currentQuestion == 0 ? "text-gry bg-gray" : "text-primary"
-              }`}
+              btnStyles={`text-lg bg-secondary shadow-md px-4 rounded-lg w-full ${currentQuestion == 0 ? "text-gry bg-gray" : "text-primary"
+                }`}
               btnClick={handlePrevious}
               disable={currentQuestion === 0}
             />
             {currentQuestion === questions.length - 1 ? (
               <Button
                 title="Submit"
-                btnStyles="bg-primary text-white text-lg rounded-lg shadow-md py-3 px-4 w-full"
+                btnStyles="bg-primary text-white text-lg rounded-lg shadow-md px-4 w-full"
                 btnClick={handleSubmit}
               />
             ) : (
               <Button
                 title={
-                  <div className="flex gap-1 items-center">
+                  <div className="flex px-4 py-3 gap-1 items-center">
                     <img src={next} className="" alt="" />
                     <span>Next</span>
                   </div>
                 }
                 btnStyles={`text-lg bg-primary shadow-md px-4 py-3 rounded-lg w-full text-white
-                        ${
-                          currentQuestion === questions.length - 1
-                            ? "hidden"
-                            : ""
-                        }`}
+                        ${currentQuestion === questions.length - 1
+                    ? "hidden"
+                    : ""
+                  }`}
                 btnClick={handleNext}
-                // disabled={currentQuestion === questions.length - 1}
+              // disabled={currentQuestion === questions.length - 1}
               />
             )}
           </div>
         </div>
-        <div className=" md:flex flex-col gap-8 w-1/2 hidden ">
+        <div className="md:flex flex-col gap-8 w-1/2 hidden">
           <div className="bg-secondary shadow-md rounded-lg py-10 items-center justify-center p-8 flex flex-col gap-8">
             <div
               className={`${timeRemaining <= 300 ? "bg-transparent text-primary" : "bg-primary text-white"} rounded-full p-6 w-24 h-24 text-center flex flex-col justify-center items-center`}
             >
               <div>Timer</div>
               <div
-                className={`text-2xl font-bold ${
-                  timeRemaining <= 300 ? "text-red-500 animate-pulse" : ""
-                }`}
+                className={`text-2xl font-bold ${timeRemaining <= 300 ? "text-red-500 animate-pulse" : ""
+                  }`}
               >
                 {Math.floor(timeRemaining / 60)}:
                 {(timeRemaining % 60).toString().padStart(2, "0")}
@@ -280,7 +286,7 @@ export const TakeExam = () => {
             onClick={() => setShowModal(true)}
           >
             <img src={quit} alt="quit" />
-            <span>Quit Test</span>
+            <span className="text-xl">Quit </span>
           </div>
         </div>
       </div>
