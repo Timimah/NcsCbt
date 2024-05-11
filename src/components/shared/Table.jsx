8b531cf9c7ec3a71c202a7aa12549d3e595453f0
Timimah <44163985+Timimah@users.noreply.github.com>
2024-05-11 20:41:16 +0100

    change table style

--- src/components/shared/Table.jsx
+++ src/components/shared/Table.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const Table = ({ data, columns }) => {
   return (
-      <div className="flex table-container w-full text-darkgrey overflow-x-scroll md:overflow-hidden shadow-md">
+      <div className="flex table-container w-full text-darkgrey overflow-x-scroll shadow-md">
         <div className="table-header flex w-1/3 justify-between font-semibold text-lg border-primary border-r-2">
           <div className={`bg-secondary w-full grid grid-cols-${columns.length}`}>
             {columns.map((column) => (
-              <div key={column.key} className="table-cell py-4 pl-7 border-b">
+              <div key={column.key} className="table-cell py-4 px-7">
                 {column.label}
               </div>
             ))}
@@ -16,7 +16,7 @@ export const Table = ({ data, columns }) => {
           {data.map((item, index) => (
             <div key={item.id} className={`w-full grid grid-cols-${columns.length} ${index % 2 === 0 ? 'bg-secondary' : ''} items-center`}>
               {columns.map((column, index) => (
-                <div key={`${item.id}-${column.key}`} className={`border-b table-cell px-5 py-5 ${index === columns.length - 2 ? 'text-primary overflow-visible overflow-x-auto' : ''} ${index === columns.length - 1 ? 'text-center' : ''}`}>
+                <div key={`${item.id}-${column.key}`} className={`border-b table-cell w-full px-5 py-5 ${index === columns.length - 2 ? 'text-primary' : ''} ${index === columns.length - 1 ? 'text-center' : ''}`}>
                   {item[column.key]}
                 </div>
               ))}
