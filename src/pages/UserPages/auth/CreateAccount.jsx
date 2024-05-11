b04c20de2dd522aaf5c9440419346b66eae24ff1
Timimah <44163985+Timimah@users.noreply.github.com>
2024-05-11 20:41:46 +0100

    add shadow

--- src/pages/UserPages/auth/CreateAccount.jsx
+++ src/pages/UserPages/auth/CreateAccount.jsx
@@ -96,7 +96,7 @@ export const CreateAccount = () => {
  
   return (
     <div className="bg-vector min-h-screen flex items-center justify-center py-10">
-      <div className='w-full max-w-lg px-10 py-8 mx-8 md:mx-auto bg-secondary rounded-2xl'>
+      <div className='w-full max-w-lg px-10 py-8 mx-8 md:mx-auto bg-secondary rounded-2xl shadow-md'>
         <div className='max-w-md mx-auto space-y-3' onSubmit={handleSubmit}>
           <h3 className="text-3xl text-primary font-bold">Welcome</h3>
           <p className="text-grey mb-8">Please enter your ID and password</p>
