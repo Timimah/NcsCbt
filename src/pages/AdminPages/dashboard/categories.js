3626a9954c16bd8c042871907a12510e92852cf7
Timimah <timimah0117@gmail.com>
2024-07-13 16:45:51 +0100

    create categories data

---
+++ src/pages/AdminPages/dashboard/categories.js
@@ -0,0 +1,14 @@
+// categories.js
+export const categories = [
+    "CAI-CAII",
+    "CAII-AIC",
+    "AIC-IC",
+    "IC-ASCII",
+    "ASCII-ASCI",
+    "ASCI-DSC",
+    "DSC-SC",
+    "SC-CSC",
+    "CSC-AC",
+    "AC-DC",
+    "DC-CC",
+  ];
