16ec30a9f3ccfca1ca3ded8aeef88ac130a70d28
Timimah <timimah0117@gmail.com>
2024-08-13 15:10:47 +0100

    add interactions

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 export const Button = ({ title, btnStyles, btnClick, type, disable }) => {
     return (
         <>
-            <button type={type} className={`relative text-center flex justify-center items-center ${btnStyles}`} onClick={btnClick} disabled={disable}>
+            <motion.button type={type} whileTap={{ scale: 0.8 }} transition={{ duration: 0.5 }}  initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1 } }} whileHover={{ scale: 1.05, boxShadow: "0px 5px 8px #016f4a" }} className={`relative text-center flex justify-center items-center ${btnStyles}`} onClick={btnClick} disabled={disable}>
                 <span className="relative">{title}</span>
-            </button>
+            </motion.button>
         </>
     )
 }
